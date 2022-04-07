52ac8a7c0659e1078db6bede24bd34bacce948a1
Adrià Estevam <adria.estevam@students.salle.url.edu>
2022-04-07 20:26:24 +0200

    Creació de les etiquetes del LoginView. Crida de les mètodes a implementar per la corresponent view i el controller.

--- src/presentation/controllers/LoginViewController.java
+++ src/presentation/controllers/LoginViewController.java
@@ -1,5 +1,7 @@
 package presentation.controllers;
 
+import business.BusinessFacadelmpl;
+import presentation.Controller;
 import presentation.views.LoginView;
 
 import java.awt.event.ActionEvent;
@@ -7,20 +9,29 @@ import java.awt.event.ActionListener;
 
 public class LoginViewController implements ActionListener {
     private LoginView loginView;
+    private Controller controller;
 
-    public LoginViewController(LoginView loginView){
+    public LoginViewController(Controller controller, LoginView loginView){
+        this.controller = controller;
         this.loginView=loginView;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         switch(e.getActionCommand()){
-            case loginView.login: //todo
+            case LoginView.BTN_LOGIN: //todo
+                if(controller.checkLoginInfo(loginView.getInfo)){
+                    controller.showHomescreenCard(controller.getPlaylist());
+                }
+                else{
+                    loginView.showLoginErrorMessage();
+                }
+                break;
+            case LoginView.BTN_FORGOTPASSWORD: //todo
+                controller.showForgotPasswordCard();
 
                 break;
-            case loginView.forgotPassword: //todo
-
-                break;
-            case loginView.signup: //todo
+            case LoginView.BTN_SINGUP: //todo
+                controller.showSignUpCard();
                 break;
         }
 
--- src/presentation/views/LoginView.java
+++ src/presentation/views/LoginView.java
@@ -1,4 +1,8 @@
 package presentation.views;
 
 public class LoginView {
+    public static final String BTN_LOGIN = "BTN_LOGIN";
+    public static final String BTN_FORGOTPASSWORD = "BTN_FORGOTPASSWORD";
+    public static final String BTN_SINGUP = "BTN_SIGNUP";
+
 }
