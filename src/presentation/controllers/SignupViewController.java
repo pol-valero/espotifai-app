7c947d7d66d7030fcd79fe512e50060326416caa
Adrià Estevam <adria.estevam@students.salle.url.edu>
2022-04-08 13:07:51 +0200

    Creació del la vista SignupView i del corresponent controller. Creació d'alguns cases dels botons.

---
+++ src/presentation/controllers/SignupViewController.java
@@ -0,0 +1,43 @@
+package presentation.controllers;
+
+import presentation.Controller;
+import presentation.views.SignupView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignupViewController implements ActionListener {
+
+    private final SignupView signupView;
+    private final Controller controller;
+
+    public SignupViewController(Controller controller, SignupView signupView){
+        this.signupView = signupView;
+        this.controller = controller;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch(e.getActionCommand()){
+            case SignupView.BTN_SIGNUP: //todo revisar el com trobem i avisem del errors.
+                int[] errors = controller.checkSingupInfo(signupView.getinfo());
+
+                if(lookForErrors(errors)){
+                    controller.showHomescreenCard(controller.getPlaylist());
+                }
+                else{
+                    signupView.showSignupErrorMessages(errors);
+                }
+                break;
+        }
+    }
+
+    public boolean lookForErrors(int[] errors) { //todo podem utilitzar aquest mètode aquí?
+        boolean flag = false;
+        for (int error : errors) {
+            if (error == 1) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+}
