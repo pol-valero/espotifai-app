7894c2aafe6fa20b9c6e95cf8267bb1d62cb6ae4
Adrià Estevam <adria.estevam@students.salle.url.edu>
2022-04-14 13:53:33 +0200

    Creació de l'esquelet del LogoutViewController. També he posat les etiquetes de la view corresponent.

---
+++ src/presentation/controllers/LogoutViewController.java
@@ -0,0 +1,29 @@
+package presentation.controllers;
+
+import presentation.Controller;
+import presentation.views.LogoutView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LogoutViewController implements ActionListener {
+    private Controller controller;
+    private LogoutView logoutView;
+
+    LogoutViewController(Controller controller, LogoutView logoutView){
+        this.controller = controller;
+        this.logoutView = logoutView;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case logoutView.BTN_LOGOUT:
+                controller.showLoginCard();
+                break;
+            case logoutView.BTN_DELETEACCOUNT:
+                controller.deleteCurrentAccount();
+                controller.showLoginCard();
+                break;
+        }
+    }
+}
