b2b0093fd7c262a2e5b5fd0b0da8d63273d6898b
joaquim <joaquim.balletbo@students.salle.url.edu>
2022-04-17 23:16:06 +0200

    començament singup view

--- src/presentation/views/SignupView.java
+++ src/presentation/views/SignupView.java
@@ -23,7 +23,7 @@ public class SignupView extends JFrame{
         Color negre = new Color(48, 48, 48);
         Color vermell = new Color (232,74,77);
         Font titols = new Font("Dialog", Font.PLAIN, 30);
-        Font text = new Font("Dialog", Font.PLAIN, 20);
+        Font text = new Font("Dialog", Font.PLAIN, 24);
         Dimension button_shape = new Dimension(371,56);
 
         JPanel p = new JPanel(new BorderLayout());
@@ -64,11 +64,53 @@ public class SignupView extends JFrame{
 
         if(error_mail){
 
-            mail_text.setToolTipText(vermell + "Wrong User name " + text);
+            mail_text.setToolTipText(vermell + "Wrong E-mail " + text);
         }
 
+        // Password
 
+        JLabel password = new JLabel();
+        password.setText("Password");
+        password.setForeground(Color.white);
+        password.setFont(titols);
 
+        // password Jtextfield
+
+        JTextField password_text = new JTextField("enter the Password", 20);
+        password_text.setFont(text);
+
+        if(error_password){
+
+            password_text.setToolTipText(vermell + "Wrong Password " + text);
+        }
+
+        // Rewrite Password
+
+        JLabel password_2 = new JLabel();
+        password_2.setText("Rewrite Password");
+        password_2.setForeground(Color.white);
+        password_2.setFont(titols);
+
+        // password_2 Jtextfield
+
+        JTextField password_2_text = new JTextField("enter the Password again", 20);
+        password_2_text.setFont(text);
+
+        if(error_password){
+
+            password_2_text.setToolTipText(vermell + "Wrong Password " + text);
+        }
+
+        //Sign up button
+        btnSignup = new JButton();
+        btnSignup.setBackground(vermell);
+        btnSignup.setForeground(Color.white);
+        btnSignup.setText("Log out");
+        btnSignup.setFont(text);
+        btnSignup.setPreferredSize(button_shape);
+        btnSignup.setFocusable(false);
+
+     //   btnSignup.setActionCommand(BTN_LOGOUT);
 
 
         p.add(imatge);
@@ -76,6 +118,12 @@ public class SignupView extends JFrame{
         p.add(name_text);
         p.add(mail);
         p.add(mail_text);
+        p.add(password);
+        p.add(password_text);
+        p.add(password_2);
+        p.add(password_2_text);
+        p.add(btnSignup);
+
 
 
 
