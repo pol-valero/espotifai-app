f9ee75332da458f74b2508da556bc50aacffc8e0
Adrià Estevam <adria.estevam@students.salle.url.edu>
2022-04-17 18:16:50 +0200

    Creació de la vista simple de LogoutView (sense listeners ni res, només visual)

--- src/presentation/views/LogoutView.java
+++ src/presentation/views/LogoutView.java
@@ -1,6 +1,82 @@
 package presentation.views;
 
-public class LogoutView {
+import javax.swing.*;
+import java.awt.*;
+
+public class LogoutView extends JFrame {
     public static final String BTN_LOGOUT = "BTN_LOGOUT";
     public static final String BTN_DELETEACCOUNT = "BTN_DELETEACCOUNT" ;
+    
+    public LogoutView () {
+        configureView();
+        configureWindows();
+    }
+
+    private void configureWindows() {
+        setTitle("Account Manager");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1512,928);
+        setLocationRelativeTo(null);
+    }
+
+    private void configureView() {
+        Color negre = new Color(48,48,48);
+        Color vermell = new Color (232,74,77);
+        Font titols = new Font("Dialog", Font.PLAIN, 36);
+        Font text = new Font("Dialog", Font.PLAIN, 24);
+        Dimension button_shape = new Dimension(371,56);
+
+        JPanel p = new JPanel(new GridBagLayout());
+        p.setBackground(negre);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(30,30,30,30);
+
+        //Title
+        JLabel title = new JLabel();
+        title.setText("Account Manager");
+        title.setForeground(Color.white);
+        title.setFont(titols);
+
+
+        //Log out btn
+        JButton btnLogout = new JButton();
+        btnLogout.setBackground(vermell);
+        btnLogout.setForeground(Color.white);
+        btnLogout.setText("Log out");
+        btnLogout.setFont(text);
+        btnLogout.setPreferredSize(button_shape);
+        btnLogout.setFocusable(false);
+
+
+
+        //Delete account btn
+        JButton btnDeleteAccount = new JButton();
+        btnDeleteAccount.setBackground(vermell);
+        btnDeleteAccount.setForeground(Color.white);
+        btnDeleteAccount.setText("Delete account");
+        btnDeleteAccount.setFont(text);
+        btnDeleteAccount.setPreferredSize(button_shape);
+        btnDeleteAccount.setFocusable(false);
+
+        //Position and addition
+        c.gridx = 0;
+        c.gridy = 1;
+        p.add(title,c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        p.add(btnLogout,c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        p.add(btnDeleteAccount,c);
+
+        getContentPane().add(p);
+
+    }
+
+
+
+
 }
