04cfa06a8290c6d797c4bc46a0aca281beb19a02
oriol <oriol.centeno@students.salle.url.edu>
2022-04-21 17:36:24 +0200

    del LoginDatabaseDAO metodo añadir usuario, del SQLConnector metodos de conexion al servidor, modificacion del config.json y tambien añadido gets a la calse usuario

--- src/persistence/DAO/LoginDatabaseDAO.java
+++ src/persistence/DAO/LoginDatabaseDAO.java
@@ -2,6 +2,7 @@ package persistence.DAO;
 
 import business.entities.User;
 import persistence.LoginDAO;
+import persistence.SQLConnector;
 
 import java.util.List;
 
@@ -14,8 +15,13 @@ public class LoginDatabaseDAO implements LoginDAO {
 
     @Override
     public void singUpRequest(User user) {
-        String query = "el codigo de la tabla y insertar ";
+        String query = "INSERT INTO usuario (Id_user, Name, Password, email) VALUES ('" +
+                "2" + "," + //el dos hay que cambiarlo por un valor qe se le asigne al usuario la id
+                user.getName() + "," +
+                user.getPassword() + "," +
+                user.getEmail() + "');";
 
+        SQLConnector.getInstance().insertQuery(query);
     }
 
     @Override
--- src/persistence/SQLConnector.java
+++ src/persistence/SQLConnector.java
@@ -4,26 +4,92 @@ import java.sql.*;
 
 public class SQLConnector {
 
-    private static SQLConnector instance = null;
+    private static SQLConnector instance = null; //empazamos sin instanciar la base
 
     private final String username;
     private final String password;
     private final String url;
-    private Connection conn;
-/* todo copiado del study cambiar a nuestra info
+    private Connection connection;
 
-    public static SQLConnector getInstance(){
+
+    public static SQLConnector getInstance(){ //si la base no esta instanciada la instanciamos
         if (instance == null ){
-            // NOT a good practice to hardcode connection data! Be aware of this for your project delivery ;)
-            instance = new SQLConnector("root", "swain", "localhost", 3306, "oopd_studentsdb"); //todo esta linea
+            instance = new SQLConnector("potify-lasalle", "3S8&jfd1", "31.193.128.38", 3306, "spotifybbdd");
             instance.connect();
         }
         return instance;
     }
- */
+
     private SQLConnector(String username, String password, String ip, int port, String database){
         this.username = username;
         this.password = password;
-        this.url = "https://spotify-lasalle.com:8443/";
+        this.url = "jdbc:mysql://" + ip + ":" + port + "/" + database;
     }
+
+    /**
+     * Metodo para conectar a la base de datos
+     */
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(url, username, password); //conecta a la base de datos usando la url
+        } catch(SQLException e) {
+            System.err.println("Couldn't connect to --> " + url + " (" + e.getMessage() + ")");
+        }
+    }
+
+    public void insertQuery(String query){
+        try {
+            Statement s = connection.createStatement();
+            s.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when inserting --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
+
+
+    public void updateQuery(String query){
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problema when updating --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
+
+
+    public void deleteQuery(String query){
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when deleting --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+
+    }
+
+
+    public ResultSet selectQuery(String query){
+        ResultSet rs = null;
+        try {
+            Statement s = connection.createStatement();
+            rs = s.executeQuery(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when selecting data --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+        return rs;
+    }
+
+
+    public void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Problem when closing the connection --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
+
 }
