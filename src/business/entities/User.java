9c19aa81d46630610ec28d6dd8610bd600a3818d
oriol <oriol.centeno@students.salle.url.edu>
2022-04-22 14:09:54 +0200

    LoginDatabaseDAO listas todas las funcions

--- src/business/entities/User.java
+++ src/business/entities/User.java
@@ -5,9 +5,21 @@ package business.entities;
  */
 public class User {
 
+    private long id;
     private String name;
     private String email;
     private String password;
+
+    public User(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
